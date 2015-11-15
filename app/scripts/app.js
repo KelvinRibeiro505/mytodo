6d4ec02c11ec4815dcab31a5aebb4d71ccb797b6
Kelvin Ribeiro <kelvin@flize.com.br>
2015-11-15 16:47:15 -0200

    refactoring app.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -47,20 +47,3 @@ angular
           }
         }); 
 }]);
-
-  // .config(function ($routeProvider) {
-  //   $routeProvider
-  //     .when('/', {
-  //       templateUrl: 'views/main.html',
-  //       controller: 'MainCtrl',
-  //       controllerAs: 'main'
-  //     })
-  //     .when('/about', {
-  //       templateUrl: 'views/about.html',
-  //       controller: 'AboutCtrl',
-  //       controllerAs: 'about'
-  //     })
-  //     .otherwise({
-  //       redirectTo: '/'
-  //     });
-  // });
